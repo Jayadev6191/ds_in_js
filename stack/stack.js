1d5d4ed975094d3dc93b70c0ebcb59a32395e0db
Jayadev6191 <jaidev.akkiraju@gmail.comm>
2017-04-30 01:50:32 -0700

    Added stack ds

---
+++ stack/stack.js
@@ -0,0 +1,24 @@
+function Stack(){
+  this.store = {};
+  this.size = 0;
+  this.limit = 3;
+}
+
+
+Stack.prototype.push = function(item) {
+  if(this.size >= this.limit) {
+    return "Max capacity already reached. Pop items before you push.";
+  }
+  this.store[++this.size] = item;
+  return Object.values(this.store);
+}
+
+Stack.prototype.pop = function() {
+  if(this.size <= 0) {
+    return "Nothing to pop";
+  }
+  var value = this.store[this.size];
+  delete this.store[this.size];
+  this.size--;
+  return Object.values(this.store)
+}
